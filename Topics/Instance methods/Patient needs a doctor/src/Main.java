7478effad9e39ec1984244c86e67cd8431b39599
Konstanty Martyniuk <constantin.radecky@gmail.com>
2021-08-28 20:52:44 +0200

    Delete Topics directory

--- Topics/Instance methods/Patient needs a doctor/src/Main.java
+++
@@ -1,8 +0,0 @@
-class Patient {
-
-    String name;
-
-    void say() {
-        System.out.printf("Hello, my name is %s, I need a doctor.", name);
-    }
-}
--- Topics/Package/Getting input/src/Main.java
+++
@@ -1,6 +0,0 @@
-class InputClass {
-    public static String getString() {
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
-        return scanner.nextLine();
-    }
-}
